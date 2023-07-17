aa7e55ecf2e87254b47effaeb3c8321cb182efe8
MioRain <ericzero1593@hotmail.com>
2023-07-17 13:20:19 +0800

    feat: add user seeder

---
+++ seeders/20230717024203-add-user-seeder.js
@@ -0,0 +1,29 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+
+function createUsersArray(userNmae, length) {
+  return Array.from({ length }, (v, i) => {
+    const name = userNmae + (i + 1).toString().padStart(3, '0')
+    return {
+      name,
+      email: name + '@example.com',
+      password: bcrypt.hashSync('titaner', bcrypt.genSaltSync(10), null),
+      role: userNmae,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+  })
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const users = createUsersArray('buyer', 10)
+    users.push(...createUsersArray('seller', 5))
+    await queryInterface.bulkInsert('Users',users, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
