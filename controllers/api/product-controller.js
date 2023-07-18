54f74daee01feb6dda84b65c684348b03d5ee62a
MioRain <ericzero1593@hotmail.com>
2023-07-18 19:51:39 +0800

    feat: add filter product feature

--- controllers/api/product-controller.js
+++ controllers/api/product-controller.js
@@ -1,9 +1,47 @@
-
+const { Op } = require("sequelize")
 const { Product, Category } = require('../../models')
 
 const productController = {
   getProducts: async (req, res) => {
+    const { filter } = req.query
+    const whereCondition = {}
+
+    if (filter?.name) {
+      whereCondition.name = {
+        [Op.like]: `%${filter.name}%`
+      }
+    }
+    if (filter?.categoryId) {
+      whereCondition.categoryId = filter.categoryId
+    }
+    if (filter?.minSize && filter?.maxSize) {
+      whereCondition.size = {
+        [Op.between]: [filter.minSize, filter.maxSize]
+      }
+    } else if (filter?.minSize) {
+      whereCondition.size = {
+        [Op.gte]: filter?.minSize
+      }
+    } else if (filter?.maxSize) {
+      whereCondition.size = {
+        [Op.lte]: filter?.maxSize
+      }
+    }
+    if (filter?.minPrice && filter?.maxPrice) {
+      whereCondition.price = {
+        [Op.between]: [filter.minPrice, filter.maxPrice]
+      }
+    } else if (filter?.minPrice) {
+      whereCondition.price = {
+        [Op.gte]: filter.minPrice
+      }
+    } else if (filter?.maxPrice) {
+      whereCondition.price = {
+        [Op.lte]: filter.maxPrice
+      }
+    }
     const products = await Product.findAll({
+      where: whereCondition,
       include: Category
     })
 
