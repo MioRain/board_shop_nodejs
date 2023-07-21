68ab01f36cc24c92092ecfb854db09fc53fa8f47
MioRain <ericzero1593@hotmail.com>
2023-07-21 19:22:47 +0800

    refactor add try catch

--- controllers/api/product-controller.js
+++ controllers/api/product-controller.js
@@ -3,59 +3,69 @@ const { Product, Category } = require('../../models')
 
 const productController = {
   getProducts: async (req, res) => {
-    const { filter } = req.query
-    const whereCondition = {}
-
-    if (filter?.name) {
-      whereCondition.name = {
-        [Op.like]: `%${filter.name}%`
+    try {
+      const { filter } = req.query
+      const whereCondition = {}
+  
+      if (filter?.name) {
+        whereCondition.name = {
+          [Op.like]: `%${filter.name}%`
+        }
       }
-    }
-    if (filter?.categoryId) {
-      whereCondition.categoryId = filter.categoryId
-    }
-    if (filter?.minSize && filter?.maxSize) {
-      whereCondition.size = {
-        [Op.between]: [filter.minSize, filter.maxSize]
+      if (filter?.categoryId) {
+        whereCondition.categoryId = filter.categoryId
       }
-    } else if (filter?.minSize) {
-      whereCondition.size = {
-        [Op.gte]: filter?.minSize
+      if (filter?.minSize && filter?.maxSize) {
+        whereCondition.size = {
+          [Op.between]: [filter.minSize, filter.maxSize]
+        }
+      } else if (filter?.minSize) {
+        whereCondition.size = {
+          [Op.gte]: filter?.minSize
+        }
+      } else if (filter?.maxSize) {
+        whereCondition.size = {
+          [Op.lte]: filter?.maxSize
+        }
       }
-    } else if (filter?.maxSize) {
-      whereCondition.size = {
-        [Op.lte]: filter?.maxSize
+      if (filter?.minPrice && filter?.maxPrice) {
+        whereCondition.price = {
+          [Op.between]: [filter.minPrice, filter.maxPrice]
+        }
+      } else if (filter?.minPrice) {
+        whereCondition.price = {
+          [Op.gte]: filter.minPrice
+        }
+      } else if (filter?.maxPrice) {
+        whereCondition.price = {
+          [Op.lte]: filter.maxPrice
+        }
       }
-    }
-    if (filter?.minPrice && filter?.maxPrice) {
-      whereCondition.price = {
-        [Op.between]: [filter.minPrice, filter.maxPrice]
-      }
-    } else if (filter?.minPrice) {
-      whereCondition.price = {
-        [Op.gte]: filter.minPrice
-      }
-    } else if (filter?.maxPrice) {
-      whereCondition.price = {
-        [Op.lte]: filter.maxPrice
-      }
-    }
-    const products = await Product.findAll({
-      where: whereCondition,
-      include: Category
-    })
-
-    res.json(products)
-  },
-  getProduct: async (req, res) => {
-    const productId = req.params.id
-    const product = await Product.findByPk(
-      productId,
-      {
+      const products = await Product.findAll({
+        where: whereCondition,
         include: Category
       })
-
-    res.json(product)
+  
+      res.json(products)
+    } catch (err) {
+      console.log(err)
+      res.json({ error: err.message })
+    }
+  },
+  getProduct: async (req, res) => {
+    try {
+      const productId = req.params.id
+      const product = await Product.findByPk(
+        productId,
+        {
+          include: Category
+        })
+  
+      res.json(product)
+    } catch (err) {
+      console.log(err)
+      res.json({ error: err.message })
+    }
   }
 }
 
