214d2d619b77da84413cdcab64dc02ecc04a342b
MioRain <ericzero1593@hotmail.com>
2023-07-26 02:59:31 +0800

    modified port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(session({ secret: 'board-shop-key', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
