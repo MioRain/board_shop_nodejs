31f912c6c618c5000b5303c1d61b4936954b1729
MioRain <ericzero1593@hotmail.com>
2023-07-26 16:32:24 +0800

    fix: * path

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiV1 = require('./modules/api/v1/api')
 
 router.use('/api/v1', apiV1)
 router.get('*', (req, res) => {
-  res.statusCode(403).json({message: 'Sorry, Not Found.'})
+  res.status(403).json({ message: 'Sorry, Not Found.' })
 })
 
 module.exports = router
